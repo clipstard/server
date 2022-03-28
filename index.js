3396e5607d91acfe7e3c977ad371387bbec96974
Nicolenco EugeniuN <eugeniu.nicolenco@tekoway.com>
2022-03-28 16:29:30 +0300

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const rawdata = fs.readFileSync(path.resolve(__dirname, 'data.json'));
@@ -160,6 +161,21 @@ app.get('/form-data', async (req, res) => {
     }
 
     res.send(formData);
+});
+
+app.get('/ping', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.send({message: 'success'});
 });
 
 app.listen(port, () => {
